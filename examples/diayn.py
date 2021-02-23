d590bae03ed2a4ca7a0704f1337b2ddf2aba80ed
naruya <naruya.kondo@gmail.com>
2021-02-23 23:29:58 +0000

    utilize

--- examples/diayn.py
+++ examples/diayn.py
@@ -1,5 +1,6 @@
 import gym
 import argparse
+import torch
 #from gym.envs.mujoco import HalfCheetahEnv
 
 import rlkit.torch.pytorch_util as ptu
@@ -14,14 +15,10 @@ from rlkit.torch.networks import FlattenMlp
 from rlkit.torch.sac.diayn.diayn_torch_online_rl_algorithm import DIAYNTorchOnlineRLAlgorithm
 
 
-def experiment(variant, args):
-    expl_env = NormalizedBoxEnv(gym.make(str(args.env)))
-    eval_env = NormalizedBoxEnv(gym.make(str(args.env)))
-    # expl_env = NormalizedBoxEnv(HalfCheetahEnv())
-    # eval_env = NormalizedBoxEnv(HalfCheetahEnv())
+def get_algorithm(expl_env, eval_env, skill_dim):
     obs_dim = expl_env.observation_space.low.size
     action_dim = eval_env.action_space.low.size
-    skill_dim = args.skill_dim
+    skill_dim = skill_dim
 
     M = variant['layer_size']
     qf1 = FlattenMlp(
@@ -88,10 +85,95 @@ def experiment(variant, args):
         replay_buffer=replay_buffer,
         **variant['algorithm_kwargs']
     )
+    log_dir = setup_logger('DIAYN_' + str(skill_dim) + '_' + str(expl_env.wrapped_env.spec)[8:-1], 
+                 variant=variant)
+    return algorithm, log_dir
+
+
+
+def get_algorithm_resume(expl_env, eval_env, skill_dim, file):
+    data = torch.load(file)
+    policy = data['evaluation/policy']
+
+    obs_dim = expl_env.observation_space.low.size
+    action_dim = eval_env.action_space.low.size
+    skill_dim = skill_dim
+
+    M = variant['layer_size']
+    qf1 = data['trainer/qf1']
+    qf2 = data['trainer/qf2']
+    target_qf1 = data['trainer/target_qf1']
+    target_qf2 = data['trainer/target_qf2']
+    df = data['trainer/df']
+    policy = data['trainer/policy']
+    eval_policy = MakeDeterministic(policy)
+    eval_path_collector = DIAYNMdpPathCollector(
+        eval_env,
+        eval_policy,
+    )
+    expl_step_collector = MdpStepCollector(
+        expl_env,
+        policy,
+    )
+    replay_buffer = DIAYNEnvReplayBuffer(
+        variant['replay_buffer_size'],
+        expl_env,
+        skill_dim,
+    )
+    trainer = DIAYNTrainer(
+        env=eval_env,
+        policy=policy,
+        qf1=qf1,
+        qf2=qf2,
+        df=df,
+        target_qf1=target_qf1,
+        target_qf2=target_qf2,
+        **variant['trainer_kwargs']
+    )
+    algorithm = DIAYNTorchOnlineRLAlgorithm(
+        trainer=trainer,
+        exploration_env=expl_env,
+        evaluation_env=eval_env,
+        exploration_data_collector=expl_step_collector,
+        evaluation_data_collector=eval_path_collector,
+        replay_buffer=replay_buffer,
+        **variant['algorithm_kwargs']
+    )
+    log_dir = setup_logger('DIAYN_' + str(skill_dim) + '_' + str(expl_env.wrapped_env.spec)[8:-1], 
+                 variant=variant)
+    return algorithm, log_dir
+
+
+def experiment(algorithm, expl_env, eval_env, args):
     algorithm.to(ptu.device)
     algorithm.train()
 
 
+# noinspection PyTypeChecker
+variant = dict(
+    algorithm="DIAYN",
+    version="normal",
+    layer_size=256,
+    replay_buffer_size=int(1E6),
+    algorithm_kwargs=dict(
+        num_epochs=1,
+        num_eval_steps_per_epoch=5000,
+        num_trains_per_train_loop=1000,
+        num_expl_steps_per_train_loop=1000,
+        min_num_steps_before_training=1000,
+        max_path_length=1000,
+        batch_size=256,
+    ),
+    trainer_kwargs=dict(
+        discount=0.99,
+        soft_target_tau=5e-3,
+        target_update_period=1,
+        policy_lr=3E-4,
+        qf_lr=3E-4,
+        reward_scale=1,
+        use_automatic_entropy_tuning=True,
+    ),
+)
 
 
 if __name__ == "__main__":
@@ -101,32 +183,11 @@ if __name__ == "__main__":
     parser.add_argument('--skill_dim', type=int, default=10,
                         help='skill dimension')
     args = parser.parse_args()
+    expl_env = NormalizedBoxEnv(gym.make(str(args.env)))
+    eval_env = NormalizedBoxEnv(gym.make(str(args.env)))
+    # expl_env = NormalizedBoxEnv(HalfCheetahEnv())
+    # eval_env = NormalizedBoxEnv(HalfCheetahEnv())
 
-    # noinspection PyTypeChecker
-    variant = dict(
-        algorithm="DIAYN",
-        version="normal",
-        layer_size=256,
-        replay_buffer_size=int(1E6),
-        algorithm_kwargs=dict(
-            num_epochs=1000,
-            num_eval_steps_per_epoch=5000,
-            num_trains_per_train_loop=1000,
-            num_expl_steps_per_train_loop=1000,
-            min_num_steps_before_training=1000,
-            max_path_length=1000,
-            batch_size=256,
-        ),
-        trainer_kwargs=dict(
-            discount=0.99,
-            soft_target_tau=5e-3,
-            target_update_period=1,
-            policy_lr=3E-4,
-            qf_lr=3E-4,
-            reward_scale=1,
-            use_automatic_entropy_tuning=True,
-        ),
-    )
-    setup_logger('DIAYN_' + str(args.skill_dim) + '_' + args.env, variant=variant)
+    algorithm, log_dir = get_algorithm(expl_env, eval_env, args.skill_dim)
     # ptu.set_gpu_mode(True)  # optionally set the GPU (default=False)
-    experiment(variant, args)
+    experiment(algorithm, expl_env, eval_env, args)
